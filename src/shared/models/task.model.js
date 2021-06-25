e2ad5f453de742509634858bb63d9bcb012a1540
payneusmc07 <45081085+payneusmc07@users.noreply.github.com>
2021-06-25 02:53:00 -0700

    Delete task.model.js

--- src/shared/models/task.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Define the structure of a task for use with the
- * task list component.
- * @see ./src/task-list/task-list.component
- * */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGFzay5tb2RlbC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInRhc2subW9kZWwudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBOzs7O0tBSUsifQ==
